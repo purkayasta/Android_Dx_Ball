27a7325cff343780c4ee6c91e4d916d101902a0f
Sunny <pritompurkayasta@gmail.com>
2017-01-08 02:07:53 +0600

    gameover Intent

--- app/src/main/java/embedded_programing_final_term_project/dx_ball/gameOver.java
+++ app/src/main/java/embedded_programing_final_term_project/dx_ball/gameOver.java
@@ -1,8 +1,21 @@
 package embedded_programing_final_term_project.dx_ball;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
 /**
  * Created by Sunny on 1/8/2017.
  */
 
-public class gameOver {
+public class gameOver extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //
+        Log.d("GameOver Class", "Again Start: ");
+        Intent i = new Intent(this,StartingGameView.class);
+        startActivity(i);
+    }
 }
